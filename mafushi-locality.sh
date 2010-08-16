1746d66fe2216b58a4e502d196e4a8599e8607c9
Thomas Weibel <thom@beeblebrox.net>
2010-08-16 22:46:41 +0200

    Add locality gnuplot scripts

---
+++ mafushi-locality.sh
@@ -0,0 +1,5 @@
+./convert "Work-Stealing Deque" results/mafushi-queues/work-stealing-deque "Linked Blocking Deque" results/mafushi-queues/linked-blocking-deque "Locality-Aware Scheduler" results/mafushi-locality/locality > gnuplot-locality/mafushi-jgf-1.dat
+
+./convert "Work-Stealing Deque" results/mafushi-queues/work-stealing-deque "Locality-Aware Scheduler" results/mafushi-locality/locality > gnuplot-locality/mafushi-jgf-2.dat
+
+./convert "Linked Blocking Deque" results/mafushi-queues/linked-blocking-deque "Locality-Aware Scheduler" results/mafushi-locality/locality > gnuplot-locality/mafushi-jgf-3.dat
