613183050f2bede21f80e27f24df0f20b9c85cce
Thomas Weibel <thom@beeblebrox.net>
2010-08-17 10:07:17 +0200

    Remove unneeded scripts

--- marvin-locality.sh
+++
@@ -1,5 +0,0 @@
-./convert "Work-Stealing Deque" results/marvin-queues/work-stealing-deque "Linked Blocking Deque" results/marvin-queues/linked-blocking-deque "Locality-Aware Scheduler" results/marvin-locality/locality > gnuplot-locality/marvin-jgf-1.dat
-
-./convert "Work-Stealing Deque" results/marvin-queues/work-stealing-deque "Locality-Aware Scheduler" results/marvin-locality/locality > gnuplot-locality/marvin-jgf-2.dat
-
-./convert "Linked Blocking Deque" results/marvin-queues/linked-blocking-deque "Locality-Aware Scheduler" results/marvin-locality/locality > gnuplot-locality/marvin-jgf-3.dat
